44ecd9141cc4c6e34205eae1b0e3a6f750083538
mengyuan <mengyuan2020@u.northwestern.edu>
2019-02-04 12:12:38 -0600

    v1

--- student_code.py
+++ student_code.py
@@ -128,7 +128,108 @@ class KnowledgeBase(object):
         printv("Retracting {!r}", 0, verbose, [fact_or_rule])
         ####################################################
         # Student code goes here
-        
+        # if a rule
+
+        if isinstance(fact_or_rule, Rule):
+            fact_or_rule = self._get_rule(fact_or_rule)
+            if fact_or_rule.asserted:
+                return
+            else:
+                if fact_or_rule in self.rules:
+                    fact_or_rule = self._get_rule(fact_or_rule)
+                    if fact_or_rule.supports_facts == [] and fact_or_rule.supports_rules == [] and fact_or_rule.supported_by == []:
+                        self.rules.remove(fact_or_rule)
+                    else:
+                        self.kb_adjust(fact_or_rule)
+
+        # if a fact
+        else:
+            fact_or_rule = self._get_fact(fact_or_rule)
+            # if asserted adjust supports_fact & support_rule first then remove
+            if not fact_or_rule.asserted:
+                return
+            else:
+                if fact_or_rule in self.facts:
+                    fact_or_rule = self._get_fact(fact_or_rule)
+                    if fact_or_rule.supports_rules == [] and fact_or_rule.supports_facts == [] and fact_or_rule.supported_by == []:
+                        self.facts.remove(fact_or_rule)
+                    else:
+                        self.kb_adjust(fact_or_rule)
+
+
+
+
+    def kb_retract2(self,fact_or_rule):
+
+        # if asserted
+        if fact_or_rule.asserted:
+            return
+        else:
+            self.kb_adjust(fact_or_rule)
+
+
+
+
+    def kb_adjust(self,f_o_r):
+
+        # adjust supported_by
+        # if f_o_r is a fact
+        if isinstance(f_o_r, Fact):
+            f_o_r = self._get_fact(f_o_r)
+            a = 0
+            for i in range(0, len(f_o_r.supported_by)):
+                    x1 = self._get_rule(f_o_r.supported_by[i][1])
+                    x2 = self._get_fact(f_o_r.supported_by[i][0])
+                    x1.supports_facts.remove(f_o_r)
+                    x2.supports_facts.remove(f_o_r)
+            #self.facts.remove(f_o_r)
+        # if f_o_r is a rule
+        else:
+            f_o_r = self._get_rule(f_o_r)
+            a = 1
+            for i in range(0, len(f_o_r.supported_by)):
+                    x1 = self._get_rule(f_o_r.supported_by[i][1])
+                    x2 = self._get_fact(f_o_r.supported_by[i][0])
+                    x1.supports_rules.remove(f_o_r)
+                    x2.supports_rules.remove(f_o_r)
+            #self.rules.remove(f_o_r)
+
+
+        # adjust supports_facts
+        for fact in f_o_r.supports_facts:
+            fact = self._get_fact(fact)
+            delete = []
+            for i in range(0, len(fact.supported_by)):
+
+                if fact.supported_by[i][a] == f_o_r:
+                    delete.append(fact.supported_by[i])
+            fact.supported_by = [x for x in fact.supported_by if x not in delete]
+            if fact.supported_by == []:
+                self.kb_retract2(fact)
+
+
+        # adjust supports_rules
+        for rule in f_o_r.supports_rules:
+            rule = self._get_rule(rule)
+            delete = []
+            for i in range(0, len(rule.supported_by)):
+
+                if rule.supported_by[i][a] == f_o_r:
+                    delete.append(rule.supported_by[i])
+            rule.supported_by = [x for x in rule.supported_by if x not in delete]
+            if rule.supported_by == []:
+                self.kb_retract2(rule)
+
+        if isinstance(f_o_r, Fact):
+            f_o_r = self._get_fact(f_o_r)
+            self.facts.remove(f_o_r)
+        else:
+            f_o_r = self._get_rule(f_o_r)
+            self.rules.remove(f_o_r)
+
+
+
+
 
 class InferenceEngine(object):
     def fc_infer(self, fact, rule, kb):
@@ -146,3 +247,31 @@ class InferenceEngine(object):
             [fact.statement, rule.lhs, rule.rhs])
         ####################################################
         # Student code goes here
+        b = match( fact.statement,rule.lhs[0])
+        if b == False:
+            return
+        if len(rule.lhs) == 0:
+            return
+        if len(rule.lhs) == 1:
+            newfact = Fact(instantiate(rule.rhs, b), [[fact,rule]])
+            #print newfact
+            rule.supports_facts.append(newfact)
+            fact.supports_facts.append(newfact)
+            kb.kb_add(newfact)
+            #newfact.supported_by.append([fact,rule])
+
+        #more than one lhs
+        else:
+            locallhs = []
+            localrule = []
+            for i in range(1, len(rule.lhs)):
+                locallhs.append(instantiate(rule.lhs[i], b))
+            localrule.append(locallhs)
+            localrule.append(instantiate(rule.rhs, b))
+            newrule = Rule(localrule,[[fact, rule]])
+            rule.supports_rules.append(newrule)
+            fact.supports_rules.append(newrule)
+            kb.kb_add(newrule)
+            #newrule.supported_by.append([fact,rule])
+
+
